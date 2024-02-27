3deff40056fb26ba4b82a0afe844beaacf2b7c4d
Misha Sanin <109426701+mikesanin@users.noreply.github.com>
2024-02-27 22:40:06 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -20,14 +20,12 @@ class Animal:
             return self.appetite
         return 0
 
-
 class Cat(Animal):
     def __init__(self, name: str, is_hungry: bool = True) -> None:
         super().__init__(name, appetite=3, is_hungry=is_hungry)
 
     def catch_mouse(self) -> None:
         print("The hunt began!")
-
 
 class Dog(Animal):
     def __init__(self, name: str, is_hungry: bool = True) -> None:
@@ -36,10 +34,6 @@ class Dog(Animal):
     def bring_slippers(self) -> None:
         print("The slippers delivered!")
 
-
 def feed_animals(animals: list[Animal]) -> int:
-    total_food = sum(animal.feed() for animal in animals)
-    return total_food
-
     total_food = sum(animal.feed() for animal in animals)
     return total_food
